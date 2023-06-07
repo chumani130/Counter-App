4427d456f831d788b3e9b7ef9e2dc63e4e736c89
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-06-06 23:48:30 -0700

    Add files via upload

---
+++ counterApp/script.js
@@ -0,0 +1,17 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+
+}
+function save(){
+    let CountStr = count + " - "
+    saveEl.textContent += CountStr
+    
+    countEl.textContent = 0
+    count = 0
+
+}
